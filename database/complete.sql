464bc0096f29d001ac597aee339c245a8d47adcc
Administrador Monteverde <adminmv@SERVERAPPS.monteverde.mtv>
2018-10-24 17:03:49 -0500

    Update.

--- database/complete.sql
+++ database/complete.sql
@@ -16,18 +16,49 @@
 CREATE DATABASE IF NOT EXISTS `inventi` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `inventi`;
 
+-- Volcando estructura para tabla inventi.areas
+DROP TABLE IF EXISTS `areas`;
+CREATE TABLE IF NOT EXISTS `areas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `icon` blob,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+
+-- Volcando datos para la tabla inventi.areas: ~4 rows (aproximadamente)
+DELETE FROM `areas`;
+/*!40000 ALTER TABLE `areas` DISABLE KEYS */;
+INSERT INTO `areas` (`id`, `name`, `icon`) VALUES
+	(1, 'Desconocida', NULL),
+	(2, 'Administrativo', NULL),
+	(3, 'Bodega', NULL),
+	(4, 'Tecnologia', NULL);
+/*!40000 ALTER TABLE `areas` ENABLE KEYS */;
+
 -- Volcando estructura para tabla inventi.articles
 DROP TABLE IF EXISTS `articles`;
 CREATE TABLE IF NOT EXISTS `articles` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `category_id` int(11) NOT NULL,
+  `area_id` int(11) NOT NULL,
+  `location_id` int(11) NOT NULL,
   `content` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `category_id` (`category_id`),
-  CONSTRAINT `articles_ibfk_3` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  KEY `area_id` (`area_id`),
+  KEY `location_id` (`location_id`),
+  CONSTRAINT `FK_articles_areas` FOREIGN KEY (`area_id`) REFERENCES `areas` (`id`),
+  CONSTRAINT `FK_articles_categories` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`),
+  CONSTRAINT `FK_articles_locations` FOREIGN KEY (`location_id`) REFERENCES `locations` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla inventi.articles: ~1 rows (aproximadamente)
+DELETE FROM `articles`;
+/*!40000 ALTER TABLE `articles` DISABLE KEYS */;
+INSERT INTO `articles` (`id`, `category_id`, `area_id`, `location_id`, `content`) VALUES
+	(1, 3, 1, 2, 'Laptop Marca DELLA');
+/*!40000 ALTER TABLE `articles` ENABLE KEYS */;
+
 -- Volcando estructura para tabla inventi.article_serials
 DROP TABLE IF EXISTS `article_serials`;
 CREATE TABLE IF NOT EXISTS `article_serials` (
@@ -41,7 +72,11 @@ CREATE TABLE IF NOT EXISTS `article_serials` (
   CONSTRAINT `article_serials_ibfk_2` FOREIGN KEY (`serial_id`) REFERENCES `serials` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla inventi.article_serials: ~0 rows (aproximadamente)
+DELETE FROM `article_serials`;
+/*!40000 ALTER TABLE `article_serials` DISABLE KEYS */;
+/*!40000 ALTER TABLE `article_serials` ENABLE KEYS */;
+
 -- Volcando estructura para tabla inventi.barcodes
 DROP TABLE IF EXISTS `barcodes`;
 CREATE TABLE IF NOT EXISTS `barcodes` (
@@ -54,7 +89,11 @@ CREATE TABLE IF NOT EXISTS `barcodes` (
   CONSTRAINT `FK_barcodes_articles` FOREIGN KEY (`article_id`) REFERENCES `articles` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla inventi.barcodes: ~0 rows (aproximadamente)
+DELETE FROM `barcodes`;
+/*!40000 ALTER TABLE `barcodes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `barcodes` ENABLE KEYS */;
+
 -- Volcando estructura para tabla inventi.categories
 DROP TABLE IF EXISTS `categories`;
 CREATE TABLE IF NOT EXISTS `categories` (
@@ -62,9 +101,17 @@ CREATE TABLE IF NOT EXISTS `categories` (
   `name` varchar(255) NOT NULL,
   `icon` blob,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla inventi.categories: ~3 rows (aproximadamente)
+DELETE FROM `categories`;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+INSERT INTO `categories` (`id`, `name`, `icon`) VALUES
+	(1, 'Hardware', NULL),
+	(2, 'Software', NULL),
+	(3, 'Otros', NULL);
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+
 -- Volcando estructura para tabla inventi.comments
 DROP TABLE IF EXISTS `comments`;
 CREATE TABLE IF NOT EXISTS `comments` (
@@ -76,7 +123,11 @@ CREATE TABLE IF NOT EXISTS `comments` (
   CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`article_id`) REFERENCES `articles` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla inventi.comments: ~0 rows (aproximadamente)
+DELETE FROM `comments`;
+/*!40000 ALTER TABLE `comments` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comments` ENABLE KEYS */;
+
 -- Volcando estructura para tabla inventi.countries
 DROP TABLE IF EXISTS `countries`;
 CREATE TABLE IF NOT EXISTS `countries` (
@@ -86,7 +137,11 @@ CREATE TABLE IF NOT EXISTS `countries` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla inventi.countries: ~0 rows (aproximadamente)
+DELETE FROM `countries`;
+/*!40000 ALTER TABLE `countries` DISABLE KEYS */;
+/*!40000 ALTER TABLE `countries` ENABLE KEYS */;
+
 -- Volcando estructura para tabla inventi.events
 DROP TABLE IF EXISTS `events`;
 CREATE TABLE IF NOT EXISTS `events` (
@@ -96,7 +151,28 @@ CREATE TABLE IF NOT EXISTS `events` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla inventi.events: ~0 rows (aproximadamente)
+DELETE FROM `events`;
+/*!40000 ALTER TABLE `events` DISABLE KEYS */;
+/*!40000 ALTER TABLE `events` ENABLE KEYS */;
+
+-- Volcando estructura para tabla inventi.locations
+DROP TABLE IF EXISTS `locations`;
+CREATE TABLE IF NOT EXISTS `locations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `icon` blob,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+
+-- Volcando datos para la tabla inventi.locations: ~2 rows (aproximadamente)
+DELETE FROM `locations`;
+/*!40000 ALTER TABLE `locations` DISABLE KEYS */;
+INSERT INTO `locations` (`id`, `name`, `icon`) VALUES
+	(1, 'Central', NULL),
+	(2, 'Robledo', NULL);
+/*!40000 ALTER TABLE `locations` ENABLE KEYS */;
+
 -- Volcando estructura para tabla inventi.serials
 DROP TABLE IF EXISTS `serials`;
 CREATE TABLE IF NOT EXISTS `serials` (
@@ -107,7 +183,11 @@ CREATE TABLE IF NOT EXISTS `serials` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla inventi.serials: ~0 rows (aproximadamente)
+DELETE FROM `serials`;
+/*!40000 ALTER TABLE `serials` DISABLE KEYS */;
+/*!40000 ALTER TABLE `serials` ENABLE KEYS */;
+
 -- Volcando estructura para tabla inventi.users
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
@@ -118,7 +198,11 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla inventi.users: ~0 rows (aproximadamente)
+DELETE FROM `users`;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
